80cdc4c7868bfea53541a72a795fda4912537a2e
Sam Bender <2336186+rednebmas@users.noreply.github.com>
2024-09-01 11:42:18 -0700

    Include class attribute when drawing stavenote

--- src/stavenote.ts
+++ src/stavenote.ts
@@ -1263,7 +1263,7 @@ export class StaveNote extends StemmableNote {
 
     // Apply the overall style -- may be contradicted by local settings:
     this.applyStyle();
-    ctx.openGroup('stavenote', this.getAttribute('id'));
+    ctx.openGroup('stavenote' + this.getAttribute('class'), this.getAttribute('id'));
     this.drawLedgerLines();
     if (shouldRenderStem) this.drawStem();
     this.drawNoteHeads();
